72fd5611a1dee18e948534c1b11b9de0ded58d5d
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-12 17:01:05 +0100

    Add PotionService and make GetAll method

---
+++ Services/PotionService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ElProyecteGrande.Interfaces.Services;
+using HogwartsPotions.Models;
+using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HogwartsPotions.Services
+{
+    public class PotionService : IPotionService
+    {
+        private readonly HogwartsContext _context;
+        public const int MaxIngredientsForPotions = 5;
+
+        public PotionService(HogwartsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Potion>> GetAll() => await _context.Potions.ToListAsync();
+    }
+}
