f24836c09edc3a9ab0958671242983bb9f51b27a
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-12 17:01:38 +0100

    Add PotionController and make endpoint to get all potion

---
+++ Controllers/PotionController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ElProyecteGrande.Interfaces.Services;
+using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PotionController : Controller
+    {
+        private readonly IPotionService _service;
+
+        public PotionController(IPotionService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Potion>>> GetAllPotion()
+        {
+            return await _service.GetAll();
+        }
+    }
+}
