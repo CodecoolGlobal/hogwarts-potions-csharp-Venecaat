44acee422fae85cceb3fedf466815ac85f9fb728
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-12 16:58:59 +0100

    Add Navigations to HogwartsContext

--- Models/HogwartsContext.cs
+++ Models/HogwartsContext.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using HogwartsPotions.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HogwartsPotions.Models
 {
     public class HogwartsContext : DbContext
     {
-        public const int MaxIngredientsForPotions = 5;
-
         public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
         {
         }
@@ -23,39 +17,12 @@ namespace HogwartsPotions.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            _ = modelBuilder.Entity<Student>().Navigation(student => student.Room).AutoInclude();
             _ = modelBuilder.Entity<Room>().Navigation(room => room.Residents).AutoInclude();
+            _ = modelBuilder.Entity<Recipe>().Navigation(recipe => recipe.Student).AutoInclude();
             _ = modelBuilder.Entity<Recipe>().Navigation(recipe => recipe.Ingredients).AutoInclude();
+            _ = modelBuilder.Entity<Potion>().Navigation(potion => potion.Student).AutoInclude();
             _ = modelBuilder.Entity<Potion>().Navigation(potion => potion.Ingredients).AutoInclude();
-        }
-
-        public async Task AddRoom(Room room)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Room> GetRoom(long roomId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Room>> GetAllRooms()
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task UpdateRoom(Room room)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task DeleteRoom(long id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<Room>> GetRoomsForRatOwners()
-        {
-            throw new NotImplementedException();
         }
     }
 }
