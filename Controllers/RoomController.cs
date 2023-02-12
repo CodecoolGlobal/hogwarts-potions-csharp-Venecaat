de72e490d614d8f588cf0f483481cb57b194fba5
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-12 17:00:20 +0100

    Refactor: empty RoomController

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -6,50 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsPotions.Controllers
 {
-    [ApiController, Route("/room")]
+    [ApiController, Route("/api/[controller]")]
     public class RoomController : ControllerBase
     {
-        private readonly HogwartsContext _context;
-
-        public RoomController(HogwartsContext context)
+        public RoomController()
         {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<List<Room>> GetAllRooms()
-        {
-            return await _context.GetAllRooms();
-        }
-
-        [HttpPost]
-        public async  Task AddRoom([FromBody] Room room)
-        {
-            await _context.AddRoom(room);
-        }
-
-        [HttpGet("/{id}")]
-        public async Task<Room> GetRoomById(long id)
-        {
-            return await _context.GetRoom(id);
-        }
-
-        [HttpPut("/{id}")]
-        public void UpdateRoomById(long id, [FromBody] Room updatedRoom)
-        {
-            _context.Update(updatedRoom);
-        }
-
-        [HttpDelete("/{id}")]
-        public async Task DeleteRoomById(long id)
-        {
-            await _context.DeleteRoom(id);
-        }
-
-        [HttpGet("/rat-owners")]
-        public async Task<List<Room>> GetRoomsForRatOwners()
-        {
-            return await _context.GetRoomsForRatOwners();
         }
     }
 }
