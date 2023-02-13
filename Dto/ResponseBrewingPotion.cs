90fea7d0bdefdcdf902aeed367245cd01a423c20
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-13 16:30:31 +0100

    Add ResponseBrewingPotion DTO

---
+++ Dto/ResponseBrewingPotion.cs
@@ -0,0 +1,25 @@
+﻿using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Models.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HogwartsPotions.Dto
+{
+    public class ResponseBrewingPotion
+    {
+        public ResponseStudent Student { get; set; }
+        public BrewingStatus BrewingStatus { get; set; }
+        public List<ResponseIngredient> Ingredients { get; set; }
+
+        public ResponseBrewingPotion MapTo(Potion potion)
+        {
+            ResponseStudent responseStudent = new ResponseStudent();
+
+            Student = responseStudent.MapTo(potion.Student);
+            Ingredients = potion.Ingredients.Select(ing => new ResponseIngredient() { Id = ing.Id, Name = ing.Name }).ToList();
+            BrewingStatus = potion.BrewingStatus;
+
+            return this;
+        }
+    }
+}
