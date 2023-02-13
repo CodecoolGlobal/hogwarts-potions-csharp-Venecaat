510367c84f28e813824f34ba6d7c3492cb865abf
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-13 14:48:07 +0100

    Add endpoint to post a new potion

--- Controllers/PotionController.cs
+++ Controllers/PotionController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using ElProyecteGrande.Interfaces.Services;
+using HogwartsPotions.Dto;
 using HogwartsPotions.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsPotions.Controllers
@@ -21,6 +23,14 @@ namespace HogwartsPotions.Controllers
         public async Task<ActionResult<List<Potion>>> GetAllPotion()
         {
             return await _service.GetAll();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<ResponsePotion>> AddNewPotion(NewPotion newPotion)
+        {
+            ResponsePotion potion = await _service.Add(newPotion);
+            return StatusCode(StatusCodes.Status201Created, potion);
         }
     }
 }
