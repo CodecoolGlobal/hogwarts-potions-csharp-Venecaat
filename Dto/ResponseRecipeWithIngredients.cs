5c9b012a4cc354379699a33da547e809ed5ba0c3
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-14 18:19:19 +0100

    Add ResponseRecipeWithIngredients DTO

---
+++ Dto/ResponseRecipeWithIngredients.cs
@@ -0,0 +1,20 @@
+﻿using HogwartsPotions.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HogwartsPotions.Dto
+{
+    public class ResponseRecipeWithIngredients : ResponseRecipe
+    {
+        public List<ResponseIngredient> Ingredients { get; set; }
+
+        public override ResponseRecipeWithIngredients MapTo(Recipe recipe)
+        {
+            Id = recipe.Id;
+            Name = recipe.Name;
+            Ingredients = recipe.Ingredients.Select(ing => new ResponseIngredient() { Id = ing.Id, Name = ing.Name }).ToList();
+
+            return this;
+        }
+    }
+}
