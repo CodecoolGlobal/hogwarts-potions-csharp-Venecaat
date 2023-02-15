cd62fd509d7a62dd0f92dd5cb8ea2b42216e5589
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-15 15:07:18 +0100

    Add MapTo method to ResponseIngredient DTO

--- Dto/ResponseIngredient.cs
+++ Dto/ResponseIngredient.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using HogwartsPotions.Models.Entities;
+
 namespace HogwartsPotions.Dto
 {
     public class ResponseIngredient
@@ -6,5 +7,13 @@ namespace HogwartsPotions.Dto
         public long Id { get; set; }
 
         public string Name { get; set; }
+
+        public ResponseIngredient MapTo(Ingredient ingredient)
+        {
+            Id = ingredient.Id;
+            Name = ingredient.Name;
+
+            return this;
+        }
     }
 }
