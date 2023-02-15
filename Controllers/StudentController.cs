13f4b250e6c2907e43b72293be0ba53614dfd675
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-15 13:04:58 +0100

    Add StudentController with endpoint to get all student

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using HogwartsPotions.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Services;
+
+namespace HogwartsPotions.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly IStudentService _service;
+
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ResponseStudent>>> GetAllStudent()
+        {
+            return await _service.GetAll();
+        }
+    }
+}
