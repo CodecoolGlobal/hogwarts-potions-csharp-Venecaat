a0a8cbb9f53c1f06d098111f0d519cf440da2084
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-15 19:09:06 +0100

    Add MapTo method to IngredientWithName DTO

--- Dto/IngredientWithName.cs
+++ Dto/IngredientWithName.cs
@@ -1,7 +1,17 @@
-﻿namespace HogwartsPotions.Dto
+﻿using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace HogwartsPotions.Dto
 {
     public class IngredientWithName
     {
         public string Name { get; set; }
+
+        public IngredientWithName MapTo(Ingredient ingredient)
+        {
+            Name = ingredient.Name;
+
+            return this;
+        }
     }
 }
