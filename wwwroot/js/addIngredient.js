ca229754a5481521c44925b7fb0f3c5a180ddaaa
DESKTOP-TV3PTH1\Renegaat <venecaat@gmail.com>
2023-02-17 14:07:45 +0100

    Update addIngredient.js and AddIngredientToPotion razor page to get help for possible potions

--- wwwroot/js/addIngredient.js
+++ wwwroot/js/addIngredient.js
@@ -2,13 +2,17 @@
 const students = document.getElementById("students");
 const ingredients = document.getElementById("ingredients");
 const submitButton = document.getElementById("submit");
+const helpButton = document.getElementById("help");
 const form = document.querySelector("form");
+const recipesList = document.getElementById("recipes-list");
 
 submitButton.addEventListener("click", showBrewingPotionForStudent);
+helpButton.addEventListener("click", showRecipes);
 
 
 async function showBrewingPotionForStudent(e) {
     event.preventDefault();
+
     let potion = await getBrewingPotionByStudentId(+students.value);
 
     if (e != undefined) {
@@ -17,7 +21,12 @@ async function showBrewingPotionForStudent(e) {
         };
         if (potion.ingredients.length < 4) {
             potion = await addIngredientToBrewingPotion(potion.id, ingredient);
+
+            if (recipesList.innerHTML !== "") showRecipes();
         }
+    }
+    else {
+        recipesList.innerHTML = "";
     }
 
     let content = "";
@@ -43,9 +52,41 @@ async function showBrewingPotionForStudent(e) {
         content += "<p class=\"ml-4 pt-2\">You don't have any brewing potion!</p>";
         submitButton.disabled = true;
     }
+
+    if (potion == null || potion.ingredients.length === 0) {
+        helpButton.disabled = true;
+    }
+    else {
+        helpButton.disabled = false;
+    }
+
     brewingPotionIngredients.innerHTML = content;
 }
 
+async function showRecipes() {
+    const potion = await getBrewingPotionByStudentId(+students.value);
+    let errorMsg = "";
+    const recipes = await getPossibleRecipes(potion.id).catch(error => { errorMsg = error; });
+    
+    let content = "";
+
+    if (recipes == undefined) content += `<p class="font-weight-bold">${errorMsg.message}</p >`;
+    else
+    {
+        for (let recipe of recipes) {
+            content += "<p>";
+            content += `<span class="font-weight-bold">${recipe.name}</span> - `;
+            for (let ingredient of recipe.ingredients) {
+                content += `${ingredient.name}, `;
+            }
+            content += "</p >";
+        }
+    }
+
+    recipesList.innerHTML = content;
+}
+
+// API Requests
 async function getBrewingPotionByStudentId(id) {
     "use strict";
     let response = await fetch(`https://localhost:44390/api/Potion/brewing/${id}`, {
@@ -70,4 +111,15 @@ async function addIngredientToBrewingPotion(potionId, payload) {
     if (response.ok) {
         return await response.json();
     }
+}
+
+async function getPossibleRecipes(potionId) {
+    "use strict";
+    let response = await fetch(`https://localhost:44390/api/Potion/${potionId}/help`, {
+        method: "GET"
+    });
+    if (response.status === 200) {
+        return await response.json();
+    }
+    return response.text().then(text => {throw new Error(text)});
 }
